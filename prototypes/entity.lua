f7e285b937247c61e35c49008ef5c8b41f07aef8
David-John Miller <anoyomouse@petermiller.co.za>
2016-06-29 20:04:49 +0200

    Changed how the warehouse entities are defined, using a function to make a single definition per warehouse and storehouse Prepped code to add in custom circuit connectors Fixed research, since we need both logistics robots and construction robots for our warehouses we've added in both as a pre-requisite

--- prototypes/entity.lua
+++ prototypes/entity.lua
@@ -3,14 +3,155 @@
  *
  * See LICENCE in the project directory for licence information
 --]]
-data:extend(
-{
-	{ -- Basic warehouse - 800 slots
-		type = "container",
-		name = "warehouse-basic",
-		icon = "__Warehousing__/graphics/icons/warehouse-basic.png",
+
+function define_warehouse_connector(mainOffset)
+	local logisticAnimationOffset = {0.015625, 0.234375};
+	local result =
+	{
+		connector_main =
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-main.png",
+			priority = "low",
+			width = 28,
+			height = 27,
+			x = 0,
+			y = 0,
+			shift = {0 + mainOffset[1], 0.015625 + mainOffset[2]},
+		},
+		led_red =
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-led-red.png",
+			priority = "low",
+			width = 20,
+			height = 16,
+			x = 0,
+			y = 0,
+			shift = {0 + mainOffset[1], -0.03125 + mainOffset[2]},
+		},
+		led_green =
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-led-green.png",
+			priority = "low",
+			width = 20,
+			height = 16,
+			x = 0,
+			y = 0,
+			shift = {0 + mainOffset[1], -0.03125 + mainOffset[2]},
+		},
+		led_blue =
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-led-blue.png",
+			priority = "low",
+			width = 21,
+			height = 22,
+			x = 0,
+			y = 0,
+			shift = {-0.015625 + mainOffset[1], 0 + mainOffset[2]},
+		},
+		logistic_animation = 
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-logistic-animation.png",
+			priority = "low",
+			blend_mode = "additive",
+			line_length = 4,
+			width = 43,
+			height = 43,
+			frame_count = 15,
+			shift = {0.015625 + logisticAnimationOffset[1] + mainOffset[1], -0.234375 + logisticAnimationOffset[2] + mainOffset[2]},
+		},
+
+		led_light =
+		{
+			intensity = 0.8,
+			size = 0.9,
+		},
+		blue_led_light_offset = {0 + mainOffset[1], -0.03125 + mainOffset[2]},
+		red_green_led_light_offset = {0 + mainOffset[1], -0.15625 + mainOffset[2]},
+	};
+
+	return result;
+end
+
+function define_storehouse_connector(mainOffset)
+	local logisticAnimationOffset = {0.015625, 0.234375};
+	local result =
+	{
+		connector_main =
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-main.png",
+			priority = "low",
+			width = 28,
+			height = 27,
+			x = 0,
+			y = 0,
+			shift = {0 + mainOffset[1], 0.015625 + mainOffset[2]},
+		},
+		led_red =
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-led-red.png",
+			priority = "low",
+			width = 20,
+			height = 16,
+			x = 0,
+			y = 0,
+			shift = {0 + mainOffset[1], -0.03125 + mainOffset[2]},
+		},
+		led_green =
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-led-green.png",
+			priority = "low",
+			width = 20,
+			height = 16,
+			x = 0,
+			y = 0,
+			shift = {0 + mainOffset[1], -0.03125 + mainOffset[2]},
+		},
+		led_blue =
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-led-blue.png",
+			priority = "low",
+			width = 21,
+			height = 22,
+			x = 0,
+			y = 0,
+			shift = {-0.015625 + mainOffset[1], 0 + mainOffset[2]},
+		},
+		logistic_animation = 
+		{
+			filename = "__Warehousing__/graphics/entity/circuit-connector/circuit-connector-logistic-animation.png",
+			priority = "low",
+			blend_mode = "additive",
+			line_length = 4,
+			width = 43,
+			height = 43,
+			frame_count = 15,
+			shift = {0.015625 + logisticAnimationOffset[1] + mainOffset[1], -0.234375 + logisticAnimationOffset[2] + mainOffset[2]},
+		},
+
+		led_light =
+		{
+			intensity = 0.8,
+			size = 0.9,
+		},
+		blue_led_light_offset = {0 + mainOffset[1], -0.03125 + mainOffset[2]},
+		red_green_led_light_offset = {0 + mainOffset[1], -0.15625 + mainOffset[2]},
+	};
+
+	return result;
+end
+
+function define_warehouse(name, logistics_name)
+	local entity_type = "container";
+	if (logistics_name ~= nil) then
+		entity_type = "logistic-container";
+	end
+	local result = 
+	{
+		type = entity_type,
+		name = "warehouse-" .. name,
+		icon = "__Warehousing__/graphics/icons/warehouse-" .. name .. ".png",
 		flags = {"placeable-neutral", "placeable-player", "player-creation"},
-		minable = {mining_time = 2, result = "warehouse-basic"},
+		minable = {mining_time = 2, result = "warehouse-" .. name},
 		max_health = 350,
 		corpse = "big-remnants",
 		dying_explosion = "medium-explosion",
@@ -24,18 +165,19 @@ data:extend(
 				percent = 90
 			}
 		},
-		collision_box = {{-2.7, -2.7}, {2.7, 2.7}}, -- {{-2.2, -2.2}, {2.2, 2.2}},
-		selection_box = {{-3.0, -3.0}, {3.0, 3.0}}, -- {{-2.5, -2.5}, {2.5, 2.5}},
+		collision_box = {{-2.7, -2.7}, {2.7, 2.7}},
+		selection_box = {{-3.0, -3.0}, {3.0, 3.0}},
 		fast_replaceable_group = "container",
 		inventory_size = 800,
 		picture =
 		{
-			filename = "__Warehousing__/graphics/entity/warehouse-basic-shadow.png",
+			filename = "__Warehousing__/graphics/entity/warehouse-" .. name .. "-shadow.png",
 			priority = "high",
 			width = 260,
 			height = 240,
 			shift = {1.0, -0.3},
 		},
+		-- circuit_connector_sprites = define_warehouse_connector({0.1875, 0.15625}),
 		circuit_wire_max_distance = 7.5,
 		circuit_wire_connection_point =
 		{
@@ -43,7 +185,6 @@ data:extend(
 			{
 				red = {2.01, 0.6},
 				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
 			},
 			wire =
 			{
@@ -51,173 +192,31 @@ data:extend(
 				green = {2.22, 0.3}
 			}
 		},
-	},
-	{ -- Passive Provider Warehouse - 800 slots
-		type = "logistic-container",
-		name = "warehouse-passive-provider",
-		icon = "__Warehousing__/graphics/icons/warehouse-passive-provider.png",
-		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "warehouse-passive-provider"},
-		max_health = 450,
-		corpse = "big-remnants",
-		collision_box = {{-2.7, -2.7}, {2.7, 2.7}}, -- {{-2.2, -2.2}, {2.2, 2.2}},
-		selection_box = {{-3.0, -3.0}, {3.0, 3.0}}, -- {{-2.5, -2.5}, {2.5, 2.5}},
-		fast_replaceable_group = "container",
-		inventory_size = 800,
-		logistic_mode = "passive-provider",
-		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-		vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		picture =
-		{
-			filename = "__Warehousing__/graphics/entity/warehouse-passive-provider-shadow.png",
-			priority = "high",
-			width = 260,
-			height = 240,
-			shift = {1.0, -0.3},
-		},
-		circuit_wire_max_distance = 7.5,
-		circuit_wire_connection_point =
-		{
-			shadow =
-			{
-				red = {2.01, 0.6},
-				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
-			},
-			wire =
-			{
-				red = {1.71, 0.3},
-				green = {2.22, 0.3}
-			}
-		},
-	},
-	{ -- Storage Warehouse - 2000 slots
-		type = "logistic-container",
-		name = "warehouse-storage",
-		icon = "__Warehousing__/graphics/icons/warehouse-storage.png",
-		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "warehouse-storage"},
-		max_health = 450,
-		corpse = "big-remnants",
-		collision_box = {{-2.7, -2.7}, {2.7, 2.7}}, -- {{-2.2, -2.2}, {2.2, 2.2}},
-		selection_box = {{-3.0, -3.0}, {3.0, 3.0}}, -- {{-2.5, -2.5}, {2.5, 2.5}},
-		fast_replaceable_group = "container",
-		inventory_size = 2000,
-		logistic_mode = "storage",
-		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-		vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		picture =
-		{
-			filename = "__Warehousing__/graphics/entity/warehouse-storage-shadow.png",
-			priority = "high",
-			width = 260,
-			height = 240,
-			shift = {1.0, -0.3},
-		},
-		circuit_wire_max_distance = 7.5,
-		circuit_wire_connection_point =
-		{
-			shadow =
-			{
-				red = {2.01, 0.6},
-				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
-			},
-			wire =
-			{
-				red = {1.71, 0.3},
-				green = {2.22, 0.3}
-			}
-		},
-	},
-	{ -- Active Provider Warehouse - 800 slots
-		type = "logistic-container",
-		name = "warehouse-active-provider",
-		icon = "__Warehousing__/graphics/icons/warehouse-active-provider.png",
-		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "warehouse-active-provider"},
-		max_health = 450,
-		corpse = "big-remnants",
-		collision_box = {{-2.7, -2.7}, {2.7, 2.7}}, -- {{-2.2, -2.2}, {2.2, 2.2}},
-		selection_box = {{-3.0, -3.0}, {3.0, 3.0}}, -- {{-2.5, -2.5}, {2.5, 2.5}},
-		fast_replaceable_group = "container",
-		inventory_size = 800,
-		logistic_mode = "active-provider",
-		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-		vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		picture =
-		{
-			filename = "__Warehousing__/graphics/entity/warehouse-active-provider-shadow.png",
-			priority = "high",
-			width = 260,
-			height = 240,
-			shift = {1.0, -0.3},
-		},
-		circuit_wire_max_distance = 7.5,
-		circuit_wire_connection_point =
-		{
-			shadow =
-			{
-				red = {2.01, 0.6},
-				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
-			},
-			wire =
-			{
-				red = {1.71, 0.3},
-				green = {2.22, 0.3}
-			}
-		},
-	},
-	{ -- Requester Warehouse - 800 slots
-		type = "logistic-container",
-		name = "warehouse-requester",
-		icon = "__Warehousing__/graphics/icons/warehouse-requester.png",
-		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "warehouse-requester"},
-		max_health = 450,
-		corpse = "big-remnants",
-		collision_box = {{-2.7, -2.7}, {2.7, 2.7}}, -- {{-2.2, -2.2}, {2.2, 2.2}},
-		selection_box = {{-3.0, -3.0}, {3.0, 3.0}}, -- {{-2.5, -2.5}, {2.5, 2.5}},
-		fast_replaceable_group = "container",
-		inventory_size = 800,
-		logistic_mode = "requester",
-		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-		vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		picture =
-		{
-			filename = "__Warehousing__/graphics/entity/warehouse-requester-shadow.png",
-			priority = "high",
-			width = 260,
-			height = 240,
-			shift = {1.0, -0.3},
-		},
-		circuit_wire_max_distance = 7.5,
-		circuit_wire_connection_point =
-		{
-			shadow =
-			{
-				red = {2.01, 0.6},
-				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
-			},
-			wire =
-			{
-				red = {1.71, 0.3},
-				green = {2.22, 0.3}
-			}
-		},
-	},
-	{ -- Basic Storehouse - 150 slots
-		type = "container",
-		name = "storehouse-basic",
-		icon = "__Warehousing__/graphics/icons/storehouse-basic.png",
+	};
+
+	if (logistics_name ~= nil) then
+		result.max_health = 450;
+		result.minable.hardness = 0.2;
+		result.logistic_mode = logistics_name;
+		if (logistics_name == "storage") then
+			result.inventory_size = 2000;
+		end
+	end
+	return result;
+end
+
+function define_storehouse(name, logistics_name)
+	local entity_type = "container";
+	if (logistics_name ~= nil) then
+		entity_type = "logistic-container";
+	end
+	local result = 
+	{
+		type = entity_type,
+		name = "storehouse-" .. name,
+		icon = "__Warehousing__/graphics/icons/storehouse-" .. name .. ".png",
 		flags = {"placeable-neutral", "placeable-player", "player-creation"},
-		minable = {mining_time = 2, result = "storehouse-basic"},
+		minable = {mining_time = 2, result = "storehouse-" .. name},
 		max_health = 250,
 		corpse = "big-remnants",
 		dying_explosion = "medium-explosion",
@@ -237,187 +236,51 @@ data:extend(
 		inventory_size = 150,
 		picture =
 		{
-			filename = "__Warehousing__/graphics/entity/storehouse-basic.png",
+			filename = "__Warehousing__/graphics/entity/storehouse-" .. name .. ".png",
 			priority = "high",
 			width = 129,
 			height = 100,
 			shift = {0.421875, 0},
 		},
+		-- circuit_connector_sprites = define_storehouse_connector({0.1875, 0.15625}),
 		circuit_wire_max_distance = 7.5,
 		circuit_wire_connection_point =
 		{
 			shadow =
 			{
-				red = {2.01, 0.6},
-				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
+				red = {.26, -0.6},
+				green = {.36, -0.6}
 			},
 			wire =
 			{
-				red = {1.71, 0.3},
-				green = {2.22, 0.3}
+				red = {-0.16, -0.9},
+				green = {0.16, -0.9}
 			}
 		},
-	},
-	{ -- Provider Storehouse - 150 slots
-		type = "logistic-container",
-		name = "storehouse-passive-provider",
-		icon = "__Warehousing__/graphics/icons/storehouse-passive-provider.png",
-		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "storehouse-passive-provider"},
-		max_health = 250,
-		corpse = "big-remnants",
-		collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-		selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-		fast_replaceable_group = "container",
-		inventory_size = 150,
-		logistic_mode = "passive-provider",
-		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-		vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		picture =
-		{
-			filename = "__Warehousing__/graphics/entity/storehouse-passive-provider.png",
-			priority = "high",
-			width = 129,
-			height = 100,
-			shift = {0.421875, 0},
-		},
-		circuit_wire_max_distance = 7.5,
-		circuit_wire_connection_point =
-		{
-			shadow =
-			{
-				red = {2.01, 0.6},
-				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
-			},
-			wire =
-			{
-				red = {1.71, 0.3},
-				green = {2.22, 0.3}
-			}
-		},
-	},
-	{ -- Active Storehouse - 150 slots
-		type = "logistic-container",
-		name = "storehouse-active-provider",
-		icon = "__Warehousing__/graphics/icons/storehouse-active-provider.png",
-		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "storehouse-active-provider"},
-		max_health = 250,
-		corpse = "big-remnants",
-		collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-		selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-		fast_replaceable_group = "container",
-		inventory_size = 150,
-		logistic_mode = "active-provider",
-		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-		vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		picture =
-		{
-			filename = "__Warehousing__/graphics/entity/storehouse-active-provider.png",
-			priority = "high",
-			width = 129,
-			height = 100,
-			shift = {0.421875, 0},
-		},
-		circuit_wire_max_distance = 7.5,
-		circuit_wire_connection_point =
-		{
-			shadow =
-			{
-				red = {2.01, 0.6},
-				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
-			},
-			wire =
-			{
-				red = {1.71, 0.3},
-				green = {2.22, 0.3}
-			}
-		},
-	},
-	{ -- Requester Storehouse - 150 slots
-		type = "logistic-container",
-		name = "storehouse-requester",
-		icon = "__Warehousing__/graphics/icons/storehouse-requester.png",
-		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "storehouse-requester"},
-		max_health = 250,
-		corpse = "big-remnants",
-		collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-		selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-		fast_replaceable_group = "container",
-		inventory_size = 150,
-		logistic_mode = "requester",
-		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-		vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		picture =
-		{
-			filename = "__Warehousing__/graphics/entity/storehouse-requester.png",
-			priority = "high",
-			width = 129,
-			height = 100,
-			shift = {0.421875, 0},
-		},
-		circuit_wire_max_distance = 7.5,
-		circuit_wire_connection_point =
-		{
-			shadow =
-			{
-				red = {2.01, 0.6},
-				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
-			},
-			wire =
-			{
-				red = {1.71, 0.3},
-				green = {2.22, 0.3}
-			}
-		},
-	},
-	{ -- Storage Storehouse - 300 slots
-		type = "logistic-container",
-		name = "storehouse-storage",
-		icon = "__Warehousing__/graphics/icons/storehouse-storage.png",
-		flags = {"placeable-player", "player-creation"},
-		minable = {hardness = 0.2, mining_time = 0.5, result = "storehouse-storage"},
-		max_health = 250,
-		corpse = "big-remnants",
-		collision_box = {{-1.2, -1.2}, {1.2, 1.2}},
-		selection_box = {{-1.5, -1.5}, {1.5, 1.5}},
-		fast_replaceable_group = "container",
-		inventory_size = 300,
-		logistic_mode = "storage",
-		open_sound = { filename = "__base__/sound/metallic-chest-open.ogg", volume=0.65 },
-		close_sound = { filename = "__base__/sound/metallic-chest-close.ogg", volume = 0.7 },
-		vehicle_impact_sound = { filename = "__base__/sound/car-metal-impact.ogg", volume = 0.65 },
-		picture =
-		{
-			filename = "__Warehousing__/graphics/entity/storehouse-storage.png",
-			priority = "high",
-			width = 129,
-			height = 100,
-			shift = {0.421875, 0},
-		},
-		circuit_wire_max_distance = 7.5,
-		circuit_wire_connection_point =
-		{
-			shadow =
-			{
-				red = {2.01, 0.6},
-				green = {2.52, 0.6}
-				-- green = {4.5, 2.6}
-			},
-			wire =
-			{
-				red = {1.71, 0.3},
-				green = {2.22, 0.3}
-			}
-		},
-	},
+	};
+
+	if (logistics_name ~= nil) then
+		result.minable.hardness = 0.2;
+		result.logistic_mode = logistics_name;
+		if (logistics_name == "storage") then
+			result.inventory_size = 300;
+		end
+	end
+	return result;
+end
+
+data:extend(
+{
+	define_warehouse("basic", nil),
+	define_warehouse("passive-provider", "passive-provider"),
+	define_warehouse("storage", "storage"),
+	define_warehouse("active-provider", "active-provider"),
+	define_warehouse("requester", "requester"),
+
+	define_storehouse("basic", nil),
+	define_storehouse("passive-provider", "passive-provider"),
+	define_storehouse("storage", "storage"),
+	define_storehouse("active-provider", "active-provider"),
+	define_storehouse("requester", "requester"),
 })
 
