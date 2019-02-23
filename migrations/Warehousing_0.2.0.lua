5eb20845e0061e69aec5a007e5db37d3c93c90ae
dgw <dgw@technobabbl.es>
2019-02-23 05:13:56 -0600

    0.2.0 migrations: rename old tech, not remove/replace
    Just gives a more accurate migration message from the game upon loading
    an old save. Won't say anything about the tech if it wasn't researched
    yet, and gives the player a better idea of what happened if it was
    (notes rename from "warehouse-logistics-research" to
    "warehouse-logistics-research-2").

--- migrations/Warehousing_0.2.0.lua
+++ migrations/Warehousing_0.2.0.lua
@@ -4,14 +4,13 @@
  * See License.txt in the project directory for license information.
 --]]
 -- migrate from old single logistic research to the new pair of techs
+-- relies on JSON migration to rename the old tech to level 2 first
 
 for index, force in pairs(game.forces) do
   local technologies = force.technologies
   local recipes = force.recipes
 
-  if recipes["warehouse-active-provider"].enabled then
-    -- with the old research, if one logistic variant was enabled, then they all were
+  if technologies["warehouse-logistics-research-2"].researched then
     technologies["warehouse-logistics-research-1"].researched = true
-    technologies["warehouse-logistics-research-2"].researched = true
   end
 end
